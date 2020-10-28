a99a01609394aa0ee67ea0fd9dd0fb22dbf18372
Precious chiedu <udegbue69@gmail.com>
2020-10-28 14:24:00 +0100

    mapped state to props

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import createBook from '../actions/index';
 
-export default class BookForm extends React.Component {
+class BookForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       category: '',
       title: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    createBook(this.state);
+  }
+
+  handleChange = e => {
+    this.setState = {
+      id: Math.random,
+      [e.target.name]: e.target.value,
     };
   }
 
@@ -19,12 +36,22 @@ export default class BookForm extends React.Component {
       'Learning',
       'Sci-Fi',
     ];
+    const {categories} = this.state
+
+    const catList = categories.map(cat => (
+      <option key={Math.random()} value={cat}>
+        {cat}
+      </option>
+    ));
+
+
     return (
       <div>
-        <form>
-          <input type="text" />
-          <select>
-            <option>options</option>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="title">Title</label>
+          <input name="title" type="text" />
+          <select name="categories" value={this.categories} onChange={this.handleChange}>
+            {catList}
           </select>
           <button type="submit">Add book</button>
         </form>
@@ -32,3 +59,9 @@ export default class BookForm extends React.Component {
     );
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  createBook: book => { dispatch(createBook(book)); },
+});
+
+export default connect(null, mapDispatchToProps)(BookForm);
