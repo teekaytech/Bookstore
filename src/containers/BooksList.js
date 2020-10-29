5af4191730561fd38757e2707a4cee6a9acb0dbf
Precious chiedu <udegbue69@gmail.com>
2020-10-29 13:53:34 +0100

    removed comment and created containers folder

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Book from './Book';
+import Book from '../components/Book';
 
 const BooksList = ({ books }) => {
   const bookList = books.map(book => (
